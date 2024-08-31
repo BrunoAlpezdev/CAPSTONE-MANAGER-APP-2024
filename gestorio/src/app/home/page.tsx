809c5e1bcd4d8814bb63b522c3fb603aaa7adf6d
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-08-31 00:11:13 -0400

    refactor: Update global CSS styles and add background image to login page

---
+++ gestorio/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return(
+  <>
+    <div>
+        <h1>hola</h1>
+    </div>
+  </>
+  )
+}
--- gestorio/src/app/page.tsx
+++ gestorio/src/app/page.tsx
@@ -1,19 +1,28 @@
 "use client"
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+
+    router.push('/home')
+
   }
 
   return (
-    <main className="flex bg-Gris h-[100dvh] items-center justify-center p-24">
-      <section className="flex flex-col border bg-Gris border-Amarillo shadow-lg p-24 rounded-lg">
+    <main className="fondo flex bg-Gris w-[100dvw] h-[100dvh] items-center justify-center p-24">
+      <section className="flex flex-col border-2 bg-Gris border-Amarillo/60 shadow-lg px-12 py-16 rounded-lg">
+        {/*
+        TODO: REVISAR BG
+         */}
         <div className="flex justify-center items-center mb-8">
           <Image
+            priority
             src="/SAVANNALOGOpng.png"
             alt="Vercel Logo"
             width={500}
@@ -23,12 +32,13 @@ export default function Home() {
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
 
-          <label >Usuario</label>
-          <input type="email" />
-          <label >Contraseña</label>
-          <input type="password" className=""/>
+          <label className="text-xl">Usuario</label>
+          <input className="bg-Verde h-9 px-2 rounded-md" type="email" />
 
-          <button className="self-center text-2xl bg-Naranjo rounded-lg w-fit px-12 py-2">
+          <label className="text-xl">Contraseña</label>
+          <input className="bg-Verde h-9 px-2 rounded-md" type="password"/>
+
+          <button className="self-center text-2xl bg-Naranjo rounded-lg w-fit px-12 py-2 mt-8 transition hover:scale-105 hover:bg-Naranjo/90">
             Iniciar Sesión
           </button>
 
