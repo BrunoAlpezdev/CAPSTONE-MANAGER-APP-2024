aa4caf12a7d7733942216ea66daeb526bf7593c5
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-09-01 20:16:10 -0400

    refactor: Update home page layout and add test paragraph

--- gestorio/src/app/(system)/home/page.tsx
+++ gestorio/src/app/(system)/home/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
-  return(
-  <div className="bg-Blanco w-full h-full">
-    
-  </div>
-  )
+	return (
+		<div className='bg-Blanco w-full h-full'>
+			<p>test</p>
+		</div>
+	)
 }
--- gestorio/src/app/(system)/layout.tsx
+++ gestorio/src/app/(system)/layout.tsx
@@ -1,28 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./system.css";
-import FooterComponent from "../components/FooterComponent";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './system.css'
+import FooterComponent from '../components/FooterComponent'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Gestorio",
-  description: "",
-};
+	title: 'Gestorio',
+	description: ''
+}
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  let a = 1
-  return (
-    <html lang="es">
-      <body className={inter.className}>
-        <header className="bg-Gris text-Blanco">header</header>
-        <main>{children}</main>
-        <FooterComponent/>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='es'>
+			<body className={inter.className}>
+				<header className='bg-Gris text-Blanco'>header</header>
+				<main>{children}</main>
+				<FooterComponent />
+			</body>
+		</html>
+	)
 }
