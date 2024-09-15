8625d91b1f6b64bd9242224f2a45babd409e3c76
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-09-15 20:22:27 -0300

    refactor: Remove unnecessary className concatenation in RootLayout component

--- gestorio/src/app/(system)/home/layout.tsx
+++ gestorio/src/app/(system)/home/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='es'>
-			<body className={inter.className + ' '}>
+			<body className={inter.className}>
 				<Header />
 				<main>{children}</main>
 				<Footer />
