1c7c33f4678e6ec54afcfdd66cfb3ce4e9cc5ce5
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-09-25 02:12:40 -0300

    feat: Add new icons for Plus, ChevronDown, VerticalDots, and Search
    This commit adds four new icon components: PlusIcon, ChevronDownIcon, VerticalDotsIcon, and SearchIcon. These icons are implemented as React components and can be used throughout the application.
    
    The PlusIcon component displays a plus sign, the ChevronDownIcon component displays a downward-pointing chevron, the VerticalDotsIcon component displays three vertical dots, and the SearchIcon component displays a magnifying glass.
    
    These icons are useful for enhancing the user interface and providing visual cues for various actions and functionalities.
    
    Refs: #123
    
    Co-authored-by: Marcelo Vivanco <Mark-soni@users.noreply.github.com>
    Co-authored-by: matias fuentes <matfuentesr@users.noreply.github.com>

--- gestorio/src/app/(system)/(SeccionesDeTablas)/layout.tsx
+++ gestorio/src/app/(system)/(SeccionesDeTablas)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import '@/styles/system-layout.css'
 import { Header, Footer } from '@components/index'
+import { NextUIProvider } from '@nextui-org/system'
+
 //import { Bug, UserRound, LogOut } from 'lucide-react' // Iconos de lucide-react
 
 export const metadata: Metadata = {
@@ -14,10 +16,14 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<>
+		/* Cada vez que quieran utilizar componentes de NextUI, 
+		deben envolver su aplicación en el <NextUIProvider> </NextUIProvider> 
+		con el contenido dentro de estos 
+		¡¡¡Esto se importa de (((( import { NextUIProvider } from '@nextui-org/system' )))) !!!*/
+		<NextUIProvider>
 			<Header />
-			{children}
+			<main>{children}</main>
 			<Footer />
-		</>
+		</NextUIProvider>
 	)
 }
