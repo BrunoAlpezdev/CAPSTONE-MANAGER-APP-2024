29e434f1e2547446e099be88e66c31b1e0a6d1db
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-09-25 12:58:12 -0300

    revert: :lipstick: removed NextUI implementation

--- gestorio/src/app/(system)/(SeccionesDeTablas)/layout.tsx
+++ gestorio/src/app/(system)/(SeccionesDeTablas)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import '@/styles/system-layout.css'
 import { Header, Footer } from '@components/index'
-import { NextUIProvider } from '@nextui-org/system'
 
 //import { Bug, UserRound, LogOut } from 'lucide-react' // Iconos de lucide-react
 
@@ -16,14 +15,10 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		/* Cada vez que quieran utilizar componentes de NextUI, 
-		deben envolver su aplicación en el <NextUIProvider> </NextUIProvider> 
-		con el contenido dentro de estos 
-		¡¡¡Esto se importa de (((( import { NextUIProvider } from '@nextui-org/system' )))) !!!*/
-		<NextUIProvider>
+		<>
 			<Header />
 			<main>{children}</main>
 			<Footer />
-		</NextUIProvider>
+		</>
 	)
 }
