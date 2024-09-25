a28a06f0aaf5ad7e15aed0f58500d14ad5891dd7
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-09-25 02:11:28 -0300

    refactor: Update dependencies and configuration files
    Co-authored-by: Ignacio Fonseca <ignFnck@users.noreply.github.com>
    Co-authored-by: Marcelo Vivanco <Mark-soni@users.noreply.github.com>
    Co-authored-by: matias fuentes <matfuentesr@users.noreply.github.com>

--- gestorio/tailwind.config.ts
+++ gestorio/tailwind.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./node_modules/@nextui-org/theme/dist/components/(checkbox|toggle).js'
+		'./node_modules/@nextui-org/theme/dist/components/(checkbox|toggle).js',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
 		extend: {
@@ -71,6 +72,7 @@ const config: Config = {
 			}
 		}
 	},
+	darkmode: 'class',
 	plugins: [nextui(), tailwindcssAnimate]
 }
 export default config
