d830047b028c420173e46c3769c63913f0ac0226
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-09-29 04:49:28 -0300

    refactor: Update package.json dependencies

--- gestorio/src/components/Table/data-table.tsx
+++ gestorio/src/components/Table/data-table.tsx
@@ -1,46 +1,177 @@
 'use client'
 
-import { ColumnDef } from '@/components/table/data-table'
-import { MoreHorizontal } from 'lucide-react'
+import {
+	ColumnDef,
+	SortingState,
+	VisibilityState,
+	flexRender,
+	ColumnFiltersState,
+	getSortedRowModel,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	useReactTable
+} from '@tanstack/react-table'
 
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '@/components/ui/table'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { Input } from '@/components/ui/input'
+
 import {
 	DropdownMenu,
+	DropdownMenuCheckboxItem,
 	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 
-export const columns: ColumnDef<Payment>[] = [
-	// ...
-	{
-		id: 'actions',
-		cell: ({ row }) => {
-			const payment = row.original
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[]
+	data: TData[]
+}
 
-			return (
+export function DataTable<TData, TValue>({
+	columns,
+	data
+}: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([])
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+	const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
+	const [rowSelection, setRowSelection] = useState({})
+
+	const table = useReactTable({
+		data,
+		columns,
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+			rowSelection
+		},
+		getCoreRowModel: getCoreRowModel(),
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		onColumnVisibilityChange: setColumnVisibility,
+		onRowSelectionChange: setRowSelection,
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		getPaginationRowModel: getPaginationRowModel()
+	})
+
+	return (
+		<>
+			{/*Table*/}
+			<div className=' flex items-center justify-between'>
+				<div className='flex items-center py-4 ml-3'>
+					<Input
+						placeholder='Filtrar Nombre'
+						value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+						onChange={(event) =>
+							table.getColumn('name')?.setFilterValue(event.target.value)
+						}
+						className='max-w-sm'
+					/>
+				</div>
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
-						<Button variant='ghost' className='h-8 w-8 p-0'>
-							<span className='sr-only'>Open menu</span>
-							<MoreHorizontal className='h-4 w-4' />
+						<Button variant='outline' className='ml-auto'>
+							Columnas
 						</Button>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent align='end'>
-						<DropdownMenuLabel>Actions</DropdownMenuLabel>
-						<DropdownMenuItem
-							onClick={() => navigator.clipboard.writeText(payment.id)}>
-							Copy payment ID
-						</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>View customer</DropdownMenuItem>
-						<DropdownMenuItem>View payment details</DropdownMenuItem>
+						{table
+							.getAllColumns()
+							.filter((column) => column.getCanHide())
+							.map((column) => {
+								return (
+									<DropdownMenuCheckboxItem
+										key={column.id}
+										className='capitalize'
+										checked={column.getIsVisible()}
+										onCheckedChange={(value) =>
+											column.toggleVisibility(!!value)
+										}>
+										{column.id}
+									</DropdownMenuCheckboxItem>
+								)
+							})}
 					</DropdownMenuContent>
 				</DropdownMenu>
-			)
-		}
-	}
-	// ...
-]
+			</div>
+			<div className='rounded-md border'>
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+													)}
+										</TableHead>
+									)
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && 'selected'}>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className='h-24 text-center'>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>{' '}
+			{/*Pagination*/}
+			<div className='flex items-center justify-end space-x-2 py-4'>
+				<Button
+					variant='outline'
+					size='sm'
+					onClick={() => table.previousPage()}
+					disabled={!table.getCanPreviousPage()}
+					className='text-black'>
+					anterior
+				</Button>
+				<Button
+					variant='outline'
+					size='sm'
+					onClick={() => table.nextPage()}
+					disabled={!table.getCanNextPage()}
+					className='text-black'>
+					siguiente
+				</Button>
+			</div>
+		</>
+	)
+}
