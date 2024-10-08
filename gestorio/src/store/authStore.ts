c2e384523f348b75da5611a194de9b72a319406f
Bruno aka bruno <102603630+Bueurro@users.noreply.github.com>
2024-10-07 21:47:15 -0300

    refactor: enhance auth store documentation and update tsconfig formatting

--- gestorio/src/store/authStore.ts
+++ gestorio/src/store/authStore.ts
@@ -12,6 +12,19 @@ interface State {
 	logout: () => Promise<void>
 }
 
+/**
+ * Zustand store for authentication management.
+ *
+ * Esta tienda maneja el estado de autenticación del usuario, incluyendo inicio de sesión, cierre de sesión y configuración del usuario.
+ *
+ * @constant
+ * @type {State}
+ *
+ * @property {User | null} user - El usuario autenticado actual, o null si no está autenticado.
+ * @property {(email: string, password: string) => Promise<any>} login - Función para iniciar sesión de un usuario con correo electrónico y contraseña.
+ * @property {(user: User) => void} setUser - Función para establecer el usuario actual.
+ * @property {() => Promise<void>} logout - Función para cerrar sesión del usuario actual.
+ */
 export const useAuthStore = create<State>((set, get) => {
 	return {
 		user: null,
